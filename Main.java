2d2da9df9c4e13ba733977a84c8ecba624ff8cbc
Sameer4215 <106610707+Sameer4215@users.noreply.github.com>
2022-06-07 12:26:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Stage1;
+//import com.project.lockedme;
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\ABC\\eclipse-workspace\\Simpli learn\\src\\Stage1"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+
+}
